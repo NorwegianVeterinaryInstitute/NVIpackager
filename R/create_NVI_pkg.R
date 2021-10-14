8d69d8a1e6c9ca168ebd09c7c169715b5085ccb2
Petter Hopp <Petter.Hopp@vetinst.no>
2021-10-14 11:12:59 +0200

    feat: draft for create_NVI_pkg

---
+++ R/create_NVI_pkg.R
@@ -0,0 +1,67 @@
+library(usethis)
+
+pkg <- stringi::stri_extract_last_words(getwd())
+
+options(
+  usethis.description = list(
+    `Authors@R` = 'c(person(given = "Petter",
+           family = "Hopp",
+           role = c("aut", "cre"),
+           email = "Petter.Hopp@vetinst.no",
+           comment = c(ORCID = "0000-0002-8695-0378")),
+    person(given = "Norwegian Veterinary Institute",
+           role = "cph"))',
+    URL = paste0("https://github.com/NorwegianVeterinaryInstitute/", pkg),
+    BugReports = paste0("https://github.com/NorwegianVeterinaryInstitute/", pkg, "/issues"),
+    License = "BSD_3_clause + file LICENSE",
+    Language =  "en-GB"
+  )
+)
+
+# Create a new package -------------------------------------------------
+# pkg <- "mypkg"
+# path <- file.path(".", pkg)
+usethis::create_package(getwd())
+
+usethis::proj_activate(path)
+
+# Modify the description ----------------------------------------------
+usethis::use_package(package = "devtools", type = "Suggests")
+usethis::use_package(package = "checkmate", type = "Imports", min_version = NULL)
+usethis::use_dev_package(package = "NVIcheckmate", type = "Imports", remote = "github::NorwegianVeterinaryInstitute/NVIcheckmate")
+
+# Set up other files -------------------------------------------------
+usethis::use_build_ignore(notes, escape = TRUE)
+
+usethis::git_vaccinate()
+usethis::use_git_ignore(ignores, directory = ".")
+
+usethis::use_readme_md()
+#> ✓ Writing 'README.md'
+
+usethis::use_news_md()
+#> ✓ Writing 'NEWS.md'
+
+usethis::use_testthat()
+
+usethis::use_code_of_conduct()
+
+usethis::use_directory(path = "notes" , ignore = FALSE)
+
+options(
+  usethis.description = list(
+    `Authors@R` = 'c(person(given = "Petter",
+           family = "Hopp",
+           role = c("aut", "cre"),
+           email = "Petter.Hopp@vetinst.no",
+           comment = c(ORCID = "0000-0002-8695-0378")),
+    person(given = "Norwegian Veterinary Institute",
+           role = "cph"))',
+    URL = paste0("https://github.com/NorwegianVeterinaryInstitute/", pkg),
+    BugReports = paste0("https://github.com/NorwegianVeterinaryInstitute/", pkg, "/issues"),
+    License = "BSD_3_clause + file LICENSE",
+    Language =  "en-GB"
+  )
+)
+
+usethis::use_vignette(name = "contribute_to_mypkg", title = "Contribute to mypkg")
