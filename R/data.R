db96963a06aef3268e5a23a3a88c23a486f9cbe5
Petter Hopp <PetterHopp@users.noreply.github.com>
2021-10-25 15:03:11 +0200

    Create data.R
    Description of NVIpackages.

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' @title Data: Description of the NVIverse packages .
+#'
+#' @description A data frame with key information for the NVIverse packages. The information is used as input to \code{README.RMD} among others. 
+#'    The code for preparing of the data frame is written in
+#'    \code{./data-raw/generate_NVIpackages.R} and the raw data can be edited there.
+#'
+#' @format A data frame with 3 variables:
+#' \describe{
+#'   \item{Package}{name of the NVIverse package.}
+#'   \item{Status}{Whether the package is "Private" or "Public".}
+#'   \item{Description}{The short description of the package as given in the "Title" field in \code{DESCRIPTION}.}
+#' }
+#' @source \code{./data-raw/generate_NVIpackages.R} in package \code{NVIpackager}
+"NVIpackages"
