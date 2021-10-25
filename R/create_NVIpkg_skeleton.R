c2c85354dd9454de91c28b104f493e9ba019628f
Petter Hopp <Petter.Hopp@vetinst.no>
2021-10-25 23:35:59 +0200

    feat: create_NVIpkg_skeleton updated Running code. Included documentation.

--- R/create_NVIpkg_skeleton.R
+++ R/create_NVIpkg_skeleton.R
@@ -1,78 +1,88 @@
-# library(usethis)
+#' @title Create the package skeleton for NVIverse packages
+#' @description Create the package skeleton for NVIverse packages. The function is
+#'     wrapper for several usethis-functions. In addition templates for standard
+#'     files are copied to the package directory.
+#'
+#' @details The function should be run after a GitHub repository has been syncronized
+#'     with the the directory and the set_description_default has been run.
+#'
+#' @param pkg The package name.
+#' @param pkg_path The path for the package
+#'
+#' @return None. Writes the DESCRIPTION and set up various directories and updates
+#'     .gitignore, .Rbuildignore, README, CONTRIBUTING, etc.
+#'
+#' @author Petter Hopp Petter.Hopp@@vetinst.no
+#' @export
+#' @examples
+#' \dontrun{
+#' # Attach packages and set up with temporary directory
+#' library(NVIpackager)
+#' td <- tempdir()
+#'
+#' }
 
 # pkg <- stringi::stri_extract_last_words(usethis::proj_path())
 
-# set up standards for DESCRIPTION file ----
-options(
-  usethis.description = list(
-    `Authors@R` = 'c(person(given = "Petter",
-           family = "Hopp",
-           role = c("aut", "cre"),
-           email = "Petter.Hopp@vetinst.no",
-           comment = c(ORCID = "0000-0002-8695-0378")),
-    person(given = "Norwegian Veterinary Institute",
-           role = "cph"))',
-    URL = paste0("https://github.com/NorwegianVeterinaryInstitute/", pkg),
-    BugReports = paste0("https://github.com/NorwegianVeterinaryInstitute/", pkg, "/issues"),
-    License = "BSD_3_clause + file LICENSE",
-    Language =  "en-GB",
-    Depends = "R (>= 4.0.0)"
-  )
-)
+create_NVIpkg_skeleton <- function(pkg = stringi::stri_extract_last_words(usethis::proj_path()),
+                                   pkg_path = usethis::proj_path()) {
 
-# Create a new package ----
-# pkg <- "mypkg"
-# path <- file.path(".", pkg)
-usethis::create_package(usethis::proj_path(), rstudio = FALSE, open = FALSE)
+  # Create standard R-package skeleton
+  usethis::create_package(path = pkg_path, rstudio = FALSE, open = FALSE)
+
+  # Modify the description ----
+  # usethis::use_package(package = "devtools", type = "Suggests")
+  usethis::use_package(package = "checkmate", type = "Imports", min_version = "2.0.0")
+  usethis::use_dev_package(package = "NVIcheckmate", type = "Imports", remote = "github::NorwegianVeterinaryInstitute/NVIcheckmate")
+
+  # Add files to .gitignore
+  usethis::use_git_ignore(ignores = "*.Rproj")
+
+  # Make extra directories
+  usethis::use_directory(path = "notes" , ignore = FALSE)
+  # Add files to .Rbuildignore
+  usethis::use_build_ignore(files = "notes", escape = TRUE)
 
 
-# Modify the description ----
-usethis::use_package(package = "devtools", type = "Suggests")
-usethis::use_package(package = "checkmate", type = "Imports", min_version = NULL)
-# usethis::use_dev_package(package = "NVIcheckmate", type = "Imports", remote = "github::NorwegianVeterinaryInstitute/NVIcheckmate")
-
-# This works
-usethis::use_git_ignore(ignores = "*.Rproj")
-# Set up other files -------------------------------------------------
-usethis::use_build_ignore(files = "notes", escape = TRUE)
+  # Prepare for vignettes ----
+  # Use Contribute_to_mypkg as the name for an example vignette. This vigntte is created later based on a template
+  usethis::use_vignette(name = paste0("Contribute_to_", pkg), title = paste("Contribute to", pkg))
 
 
-# SET UP VIGNETTES ----
-usethis::use_vignette(name = paste0("Contribute_to_", pkg), title = paste("Contribute to", pkg))
-use_contribute_to_NVIpkg()
+  # set up test structure ----
+  usethis::use_testthat()
+  # spell check ask if shall overwrite file
+  # usethis::use_spell_check(vignettes = TRUE, lang = "en-GB", error = FALSE)
+  # spelling::spell_check_setup <- function(pkg = ".",
+  #                               vignettes = TRUE,
+  #                               lang = "en-GB",
+  #                               error = FALSE)
 
-# INCLUDE TEMPLATES ----
-# README.Rmd
-
-# NEWS.md
-# usethis::use_template("NEWS.md",
-#   data = package_data(),
-#   open = open
-# )
-# Seems like changes must be committed before use_news_md() is run
-# usethis::use_news_md(open = FALSE)
+  # usethis::use_coverage()
 
 
 
 
-# set up test structure ----
-usethis::use_testthat()
-# spell check ask if shall overwrite file
-# usethis::use_spell_check(vignettes = TRUE, lang = "en-GB", error = FALSE)
-# spelling::spell_check_setup <- function(pkg = ".",
-#                               vignettes = TRUE,
-#                               lang = "en-GB",
-#                               error = FALSE)
+  # INCLUDE TEMPLATES ----
+  usethis::use_code_of_conduct()
 
-usethis::use_coverage()
+  update_contribute()
+
+    # README.Rmd
+
+  # NEWS.md
+  # usethis::use_template("NEWS.md",
+  #   data = package_data(),
+  #   open = open
+  # )
+  # Seems like changes must be committed before use_news_md() is run
+  # usethis::use_news_md(open = FALSE)
 
 
 
-usethis::use_directory(path = "notes" , ignore = FALSE)
+  # description_titel <- desc::desc_get_field(key = "Title")
+  # description_descr <- desc::desc_get_field(key = "Description")
 
-
-# description_titel <- desc::desc_get_field(key = "Title")
-# description_descr <- desc::desc_get_field(key = "Description")
-
-# use_logo(img, geometry = "240x278", retina = TRUE)
-# use_package_doc(open = FALSE)
+  # use_logo(img, geometry = "240x278", retina = TRUE)
+  # use_package_doc(open = FALSE)
+}
