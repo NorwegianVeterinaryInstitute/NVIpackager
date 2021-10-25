c6d8b1cc0fbed3669038153c2dc3406c08a5f62a
Petter Hopp <Petter.Hopp@vetinst.no>
2021-10-25 23:34:18 +0200

    refacter: code from use_contribute_to_NVIpkg included in update_contribute

--- R/use_contribute_to_NVIpkg.R
+++
@@ -1,7 +0,0 @@
-use_contribute_to_NVIpkg <- function(pkg = stringi::stri_extract_last_words(getwd())) {
-  usethis::use_vignette(name = paste0("Contribute_to_", pkg), title = paste("Contribute to", pkg))
-  file.copy(from = "./inst/Rmd/Contribute_to_NVIpkg.Rmd",
-            to = paste0("./vignettes/Contribute_to_", pkg, ".Rmd"),
-            overwrite = TRUE)
-}
-
