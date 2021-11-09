010026378f040437b6d949de6e9aa5606ff1012f
Petter Hopp <Petter.Hopp@vetinst.no>
2021-11-09 15:42:37 +0100

    style: update_readme with Ctrl+I

--- R/update_readme.R
+++ R/update_readme.R
@@ -1,15 +1,15 @@
 #' @title Update \code{README.md}
-#' @description Update \code{README.md} 
+#' @description Update \code{README.md}
 #'     from \code{README.Rmd} and child templates in \code{NVIpackager}. If the file don't exist, it is created.
 #'
-#' @details The child templates are found in \code{NVIpackager}. Any change in this 
+#' @details The child templates are found in \code{NVIpackager}. Any change in this
 #'     text must be done in the templates.
 #'
-#' @template pkg 
-#' @template pkg_path 
+#' @template pkg
+#' @template pkg_path
 #'
 #' @return None.
-#'     Writes the file \code{README.md}. 
+#'     Writes the file \code{README.md}.
 #'
 #' @author Petter Hopp Petter.Hopp@@vetinst.no
 #' @export
@@ -36,10 +36,7 @@ update_readme <- function(pkg = stringi::stri_extract_last_words(usethis::proj_p
   # RUN SCRIPT ----
   # For updating README.md.
   rmarkdown::render(input = paste0(pkg_path, "/README.Rmd"),
-                    output_format = "md_document",
+                    # output_format = "md_document",
                     output_file = "README.md",
                     output_dir = pkg_path)
-  header <- paste0("# READ ME" , "\n")
-  writeLines(c(header,readLines(paste0(pkg_path, "/README.md"))),
-             paste0(pkg_path, "/README.md"))
 }
