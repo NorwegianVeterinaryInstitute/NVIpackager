9b9e1f4d3e20312c97d0445453ae9bc1e6e5606e
Petter Hopp <Petter.Hopp@vetinst.no>
2021-11-13 22:39:33 +0100

    fix: install_NVIpkg Corrected input to assert_pkg_path. Changed to argument rsource.

--- R/install_NVIpkg.R
+++ R/install_NVIpkg.R
@@ -4,7 +4,7 @@
 #'     package during development to test new code in the package
 #'     scripts .
 #'
-#' @details When \code{source = "github"}, the default is to install
+#' @details When \code{rsource = "github"}, the default is to install
 #'     the latest version in the main branch at the NorwegianVeterinaryInstitute
 #'     package repository. During package development one will usually
 #'     change the username to your own.
@@ -15,9 +15,9 @@
 #'     of the arguments \code{repo =}, \code{username =}, and
 #'     \code{ref =}.
 #'
-#'     When \code{source = "local"}, it installs the package from a
+#'     When \code{rsource = "local"}, it installs the package from a
 #'     local copy of the package repository. It defaults to install
-#'     the active branch. Use \code{source = "local"} to test new
+#'     the active branch. Use \code{rsource = "local"} to test new
 #'     code during development.
 #'
 #'     For installing the latest released versions of NVIverse
@@ -29,7 +29,7 @@
 #' @param lib character giving the library directory where to install
 #'     the package. If missing, defaults to the first element of
 #'     \code{.libPaths()}.
-#' @param source, character with one of c("github", "local") .
+#' @param rsource, character with one of c("github", "local") .
 #' @param username, the github username to install from. Defaults to
 #'     "NorwegianVeterinaryInstitute".
 #' @param \dots Other arguments to be passed to \code{remotes::install_github}
@@ -44,10 +44,10 @@
 #' # Reading from standard directory at NVI's network
 #' }
 #'
-install_NVIpkg <- function(pkg,
-                           pkg_path,
+install_NVIpkg <- function(pkg = stringi::stri_extract_last_words(usethis::proj_path()),
+                           pkg_path = usethis::proj_path(),
                            lib = R.home(),
-                           source,
+                           rsource,
                            username = "NorwegianVeterinaryInstitute",
                            ...) {
 
@@ -55,9 +55,9 @@ install_NVIpkg <- function(pkg,
   # Object to store check-results
   checks <- checkmate::makeAssertCollection()
   # Perform checks
-  assert_pkg_path()
+  assert_pkg_path(pkg = pkg, pkg_path = pkg_path)
   checkmate::assert_directory_exists(x = lib, add = checks)
-  checkmate::assert_character(x = source, add = checks)
+  checkmate::assert_character(x = rsource, add = checks)
   checkmate::assert_character(x = username, add = checks)
   # Report check-results
   checkmate::reportAssertions(checks)
@@ -72,7 +72,7 @@ install_NVIpkg <- function(pkg,
 
   # INSTALL PACKAGE ----
   # Install from working directory
-  if(source == "local") {
+  if(rsource == "local") {
     withr::with_libpaths(paste0(lib,"/library"),
                          devtools::install(pkg_path,
                                            dependencies = c("Depends", "Imports", "LinkingTo"),
@@ -82,7 +82,7 @@ install_NVIpkg <- function(pkg,
     )
   }
 
-  if(source == "github") {
+  if(rsource == "github") {
     # Install from Github repository
     remotes::install_github(paste0(username, "/", pkg),
                             upgrade = FALSE,
