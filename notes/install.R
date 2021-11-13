3fd14ff6c875065fe7d6fd63bcc4249a6c5c3193
Petter Hopp <Petter.Hopp@vetinst.no>
2021-11-13 22:40:18 +0100

    refactor: install.R simplified with NVIpackager functions

--- notes/install.R
+++ notes/install.R
@@ -3,48 +3,36 @@
 # library(devtools)
 
 pkg <- stringi::stri_extract_last_words(usethis::proj_path())
+pkg_path = usethis::proj_path()
 
-Rlibrary <- R.home()
+NVIpackager::install_NVIpkg(pkg = pkg, pkg_path = pkg_path, rsource = "local")
 
-library(withr)
-
-# DETACH PACKAGE ----
-# The package must be detached to install it.
-if(pkg %in% (.packages())){
-  pkgname <- paste0("package:", pkg)
-  detach(pkgname, unload=TRUE, character.only = TRUE)
-}
+# NVIpackager::install_NVIpkg(pkg = pkg, pkg_path = pkg_path, rsource = "github", username = "PetterHopp")
+#
+# NVIpackager::install_NVIpkg(pkg = pkg, pkg_path = pkg_path, rsource = "github", username = "NorwegianVeterinaryInstitute")
+#
 
 
-# INSTALL PACKAGE ----
-# Install from working directory
-withr::with_libpaths(paste0(Rlibrary,"/library"),
-                     devtools::install(getwd(),
-                                       dependencies = c("Depends", "Imports", "LinkingTo"),
-                                       upgrade=FALSE,
-                                       build_vignettes = TRUE)
-)
+# # Install from NorwegianVeterinaryInstitute at GitHub
+# remotes::install_github(paste0("NorwegianVeterinaryInstitute/", pkg),
+#                         upgrade = FALSE,
+#                         build = TRUE,
+#                         build_manual = TRUE,
+#                         build_vignettes = TRUE)
+#
+#
+# # Install from personal repository at GitHub
+# remotes::install_github(paste0("PetterHopp/", pkg),
+#                         upgrade = FALSE,
+#                         build = TRUE,
+#                         build_manual = TRUE,
+#                         build_vignettes = TRUE)
+#
 
-# Install from NorwegianVeterinaryInstitute at GitHub
-remotes::install_github(paste0("NorwegianVeterinaryInstitute/", pkg),
-                        upgrade = FALSE,
-                        build = TRUE,
-                        build_manual = TRUE,
-                        build_vignettes = TRUE)
-
-
-# Install from personal repository at GitHub
-remotes::install_github(paste0("PetterHopp/", pkg),
-                        upgrade = FALSE,
-                        build = TRUE,
-                        build_manual = TRUE,
-                        build_vignettes = TRUE)
-
-
-# # Install from source file in catalog "NVIverse"
-utils::install.packages(pkgs = paste0(NVIconfig:::path_NVI["NVIverse"], "/", pkg, "/Arkiv/", pkg, "_", version, ".tar.gz"),
-                        repos = NULL,
-                        type = "source")
+# # # Install from source file in catalog "NVIverse"
+# utils::install.packages(pkgs = paste0(NVIconfig:::path_NVI["NVIverse"], "/", pkg, "/Arkiv/", pkg, "_", version, ".tar.gz"),
+#                         repos = NULL,
+#                         type = "source")
 #
 # # Install from binary file in catalog "NVIverse"
 # install.packages(pkgs = paste0(NVIconfig:::path_NVI["NVIverse"], "/", pkg, "/Arkiv/", pkg, "_", version, ".zip"),
