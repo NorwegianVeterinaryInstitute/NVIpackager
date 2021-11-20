48ab210e86e1daf1287af3f5792556f7d30f12d3
Petter Hopp <PetterHopp@users.noreply.github.com>
2021-11-20 15:38:39 +0100

    feat: document_NVIpkg updated
    Included the possibilty of adding scope as an argument.
    Changed default value to "spaces". 
    Updated help.

--- R/document_NVIpkg.R
+++ R/document_NVIpkg.R
@@ -5,6 +5,11 @@
 #' @details The help files for R-functions will always be generated. Whether
 #'     styling should be performed and CONTRIBUTING.md and README
 #'     should be updated are controlled by input arguments.
+#' 
+#'     Default value for styling is \code{scope = "spaces"}. Input to
+#'     \code{scope} can be any subset of c("spaces", "indention", "line_breaks", "tokens"). 
+#'     For indention, rather use \code{Ctrl+I} than \code{scope = "indention"}.
+#'     Be careful if using \code{scope = "tokens"} as code may be broken. 
 #'
 #' @template pkg
 #' @template pkg_path
@@ -15,8 +20,8 @@
 #'     updated, defaults to \code{FALSE}.
 #' @param readme \[\code{logical}\]\cr
 #'     Should \code{README} be updated, defaults to \code{FALSE}.
-#' @param \dots	Other arguments to be passed to \code{styler::style_pkg} .
-#'
+#' @param \dots	Other arguments to be passed to \code{styler::style_pkg}. 
+#' 
 #' @return none. Updated help files for all functions and,
 #'     depending on argument input, can updated style,
 #'     \code{CONTRIBUTING.md}, and \code{README}
@@ -36,11 +41,11 @@
 #'   dir.create(file.path(td, "NVItest", "vignettes"))
 #' }
 #'
-#' update_contributing(pkg = "NVItest",
-#'                     pkg_path = file.path(td, "NVItest"),
-#'                     style = FALSE,
-#'                     contributing = TRUE,
-#'                     readme = TRUE)
+#' document_NVIpkg(pkg = "NVItest",
+#'                 pkg_path = file.path(td, "NVItest"),
+#'                 style = FALSE,
+#'                 contributing = TRUE,
+#'                 readme = TRUE)
 #' }
 #'
 document_NVIpkg <- function(pkg = stringi::stri_extract_last_words(usethis::proj_path()),
@@ -57,12 +62,20 @@ document_NVIpkg <- function(pkg = stringi::stri_extract_last_words(usethis::proj
   checkmate::assert_logical(x = style, add = checks)
   checkmate::assert_logical(x = contributing, add = checks)
   checkmate::assert_logical(x = readme, add = checks)
-  # Report check-results
+  if (exists(scope)) {
+    NVIcheckmate::assert_subset_character(x = scope, 
+                                        choices = c("spaces", "indention", "line_breaks", "tokens"), 
+                                        add = checks)
+} 
+# Report assertions 
   checkmate::reportAssertions(checks)
 
   if (isTRUE(style)) {
+    if (!exists(scope)) {
+      scope <- "spaces" 
+} 
     styler::style_pkg(pkg = pkg_path,
-                      transformers = styler::tidyverse_style(scope = I(c("spaces", "indention", "tokens"))),
+                      transformers = styler::tidyverse_style(scope = I(c(scope))),
                       ...)
   }
 
