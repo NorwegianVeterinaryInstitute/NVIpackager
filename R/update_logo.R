550bfe1fd6734ccb29404b2b65d55cc11cceb589
Petter Hopp <petter.hopp@vetinst.no>
2022-03-28 19:02:46 +0200

    feat: created update_logo

---
+++ R/update_logo.R
@@ -0,0 +1,43 @@
+#' @title Update logo
+#' @description Update the logo to be used in the README file.
+#'
+#' @details If the package logo has been created, the logo is copied from
+#'     \code{NVIrpackages} to the package directory "./man/figures/. If the logo
+#'     already exists, the logo is overwritten.
+#'
+#' @template pkg
+#' @template pkg_path
+#'
+#' @return None. Copies the the logo file to the directory "./man/figures/.
+#'
+#' @author Petter Hopp Petter.Hopp@@vetinst.no
+#' @export
+#' @examples
+#' \dontrun{
+#' # Attach packages and set up with temporary directory
+#' library(NVIpackager)
+#' update_logo()
+#' }
+update_logo <- function(pkg = stringi::stri_extract_last_words(usethis::proj_path()),
+                        pkg_path = usethis::proj_path()) {
+
+  # ARGUMENT CHECKING ----
+  # Object to store check-results
+  checks <- checkmate::makeAssertCollection()
+  # assertions
+  assert_pkg_path(pkg = pkg, pkg_path = pkg_path)
+  # Report check-results
+  checkmate::reportAssertions(checks)
+
+  # RUN SCRIPT ----
+  # Set up vignettes
+  if (file.exists(system.file(paste0('logos/', pkg, "_logo.png"), package = "NVIrpackages"))) {
+    if (!dir.exists(paths = file.path(pkg_path, "man"))) {
+      dir.create(file.path(pkg_path, "man"))
+    }
+    if (!dir.exists(file.path(pkg_path, "man/figures"))) {
+      dir.create(file.path(pkg_path, "man/figures"))
+    }
+    file.copy(from = system.file(paste0('logos/', pkg, "_logo.png"), package = "NVIrpackages"), to = file.path(pkg_path, "man/figures"), overwrite = TRUE)
+  }
+}
