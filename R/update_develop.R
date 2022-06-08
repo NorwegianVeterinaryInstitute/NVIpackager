329a5213e77a69c3c06e5efbbfc275915ce5f83e
Petter Hopp <petter.hopp@vetinst.no>
2022-06-08 18:56:07 +0200

    feat: created update_develop and template/develop.R

---
+++ R/update_develop.R
@@ -0,0 +1,48 @@
+#' @title Update \code{develop.R}
+#' @description Update \code{develop.R} from the template in \code{NVIpackager}.
+#'     If the file already exist, it is overwritten.
+#'
+#' @details develop.R is a script comprising code for creating, documenting,
+#'     testing and installing a package during development and maintainance. All
+#'     code should be written without reference to a specific package, so that
+#'     the script can be used without modification in development and maintainance
+#'     of all NVIverse packages.
+#'
+#'     The template is found in \code{NVIpackager}. Any change in the script
+#'     should be done in the template. Thereby, it is easier to keep the script
+#'     updated in all packages.
+#'
+#' @template pkg
+#' @template pkg_path
+#'
+#' @return None. Writes the R script \code{develop.R} to ./notes.
+#'
+#' @author Petter Hopp Petter.Hopp@@vetinst.no
+#' @export
+#' @examples
+#' \dontrun{
+#' # Updates the script develop.R
+#' update_develop()
+#' }
+#'
+update_develop <- function(pkg = stringi::stri_extract_last_words(usethis::proj_path()),
+                                pkg_path = usethis::proj_path()) {
+
+  # ARGUMENT CHECKING ----
+  # Object to store check-results
+  checks <- checkmate::makeAssertCollection()
+  # assertions
+  assert_pkg_path(pkg = pkg, pkg_path = pkg_path)
+  # Report check-results
+  checkmate::reportAssertions(checks)
+
+  # RUN SCRIPT ----
+  # Create ./notes if it doesn't exist
+  if (!dir.exists(file.path(pkg_path, "notes"))) {
+    dir.create(path = file.path(pkg_path, "notes"))
+  }
+  # Copy develop.R from template to ./notes
+  file.copy(from = system.file('templates', "develop.R", package = "NVIpackager"),
+            to = file.path(pkg_path, "notes", "develop.R"),
+            overwrite = TRUE)
+}
---
+++ inst/templates/develop.R
@@ -0,0 +1,84 @@
+# CREATE, DOCUMENT, TEST AND INSTALL THE PACKAGE
+
+# Update this file with the template in NVIpackager
+NVIpackager::update_develop()
+
+# SET UP ENVIRONMENT ----
+# rm(list = ls())    # Use this to empty the environment
+
+# Attach packages
+library(NVIpackager)
+# library(spelling)
+
+# Global variables
+pkg_path = usethis::proj_path()
+pkg <- stringi::stri_extract_last_words(pkg_path)
+
+
+# CREATE PACKAGE SKELETON ----
+# create_NVIpkg_skeleton(license_keyword = "CC BY 4.0")
+
+
+# DOCUMENTATION AND STYLING ----
+# update_loge should be run if a logo has been created (or updated). Thereafter use "readme = TRUE"
+# update_logo(pkg = pkg, pkg_path = pkg_path)
+
+# Creates new help files
+# Should be run before git push when documentation for functions have been changed
+NVIpackager::document_NVIpkg(pkg = pkg,
+                             pkg_path = pkg_path,
+                             style = TRUE,
+                             manual = "update",
+                             contributing = FALSE,
+                             readme = FALSE,
+                             scope = c("spaces", "line_breaks"))
+
+# spelling::spell_check_package(vignettes = TRUE, use_wordlist = TRUE)
+
+
+# TEST PACKAGE ----
+# Run tests included in ./tests.
+devtools::test()
+
+# Test package coverage
+# The package must be detached to install it.
+if(pkg %in% (.packages())){
+  pkgname <- paste0("package:", pkg)
+  detach(pkgname, unload=TRUE, character.only = TRUE)
+}
+# Test and print package coverage
+code_coverage <- covr::package_coverage(path = ".", group = "functions")
+print(x = code_coverage, group = "functions")
+
+# Build the package and thereafter check
+# Thereby, no problems with files in .Rbuildignore.
+devtools::build(binary = FALSE, manual = TRUE, vignettes = TRUE)
+version <- utils::packageVersion(pkg, lib.loc = paste0(pkg_path,"/.."))
+# Test built package
+devtools::check_built(path = paste0("../", pkg, "_", version, ".tar.gz"), args = c("--no-tests"), manual = TRUE)
+
+
+# INSTALL PACKAGE ----
+# From local version
+NVIpackager::install_NVIpkg(pkg = pkg, pkg_path = pkg_path, rsource = "local")
+# From GitHub
+# NVIpackager::install_NVIpkg(pkg = pkg, pkg_path = pkg_path, rsource = "github", username = "PetterHopp")
+#
+# NVIpackager::install_NVIpkg(pkg = pkg, pkg_path = pkg_path, rsource = "github", username = "NorwegianVeterinaryInstitute")
+#
+# # # Install from source file in catalog "NVIverse"
+# utils::install.packages(pkgs = paste0(NVIconfig:::path_NVI["NVIverse"], "/", pkg, "/Arkiv/", pkg, "_", version, ".tar.gz"),
+#                         repos = NULL,
+#                         type = "source")
+#
+# # Install from binary file in catalog "NVIverse"
+# install.packages(pkgs = paste0(NVIconfig:::path_NVI["NVIverse"], "/", pkg, "/Arkiv/", pkg, "_", version, ".zip"),
+#                  repos = NULL,
+#                  type = "binary")
+
+# ATTACH PACKAGE ----
+utils::help(package = (pkg))
+
+library(package = pkg, character.only = TRUE)
+
+
