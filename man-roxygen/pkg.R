db7e35a15fbd88e00ed689bdb9139c8fa757578a
Petter Hopp <petter.hopp@vetinst.no>
2022-08-19 06:40:34 +0200

    doc: man-oxygen/pkg.R updated. Rewritten for using roxygen2 without markdown.

--- man-roxygen/pkg.R
+++ man-roxygen/pkg.R
@@ -1,2 +1,2 @@
-#' @param pkg \[\code{character}\]\cr
-#'   The package name, defaults to \code{stringi::stri_extract_last_words(usethis::proj_path())}. 
+#' @param pkg [\code{character}]\cr
+#'   The package name, defaults to \code{stringi::stri_extract_last_words(usethis::proj_path())}.
