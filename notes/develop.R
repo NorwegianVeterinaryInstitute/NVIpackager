1beed15724986fb56aabd94fb676a389d6086bdb
Petter Hopp <Petter.Hopp@vetinst.no>
2023-08-29 07:56:49 +0200

    refactor: updated develop.R from template

--- notes/develop.R
+++ notes/develop.R
@@ -16,13 +16,19 @@ pkg <- stringi::stri_extract_last_words(pkg_path)
 
 
 # CREATE PACKAGE SKELETON ----
-# create_NVIpkg_skeleton(license_keyword = "CC BY 4.0")
+# create_NVIpkg_skeleton(license_keyword = "BSD_3_clause") # BSD_3_clause # CC BY 4.0
 
 # INCREASE PACKAGE VERSION IN DESCRIPTION AND NEWS ----
 # NVIpackager::increase_NVIpkg_version(pkg = pkg,
 #                                   pkg_path = pkg_path,
 #                                   type = "develop",
 #                                   document = FALSE)
+
+# UPDATE LICENSE
+# NVIpackager::update_license(pkg = pkg,
+#                             pkg_path = pkg_path,
+#                             copyright_owner = "Norwegian Veterinary Institute")
+
 
 # DOCUMENTATION AND STYLING ----
 # update_logo should be run if a logo has been created (or updated). Thereafter run "document_NVIpkg" with "readme = TRUE".
@@ -37,6 +43,8 @@ NVIpackager::document_NVIpkg(pkg = pkg,
                              readme = FALSE,
                              manual = "update",
                              scope = c("spaces", "line_breaks"))
+# filename <- "xxxx.R"
+# styler::style_file(path = file.path(pkg_path, "R", filename), scope = I(c("spaces")))
 
 # spelling::spell_check_package(vignettes = TRUE, use_wordlist = TRUE)
 
