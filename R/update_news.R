3d490ea6492f89b1e50834fbc18a55e5108742fb
Petter Hopp <Petter.Hopp@vetinst.no>
2024-04-12 07:26:52 +0200

    feat: updated update_news to use NEWS.md

--- R/update_news.R
+++ R/update_news.R
@@ -1,25 +1,25 @@
-#' @title Update \code{NEWS}
-#' @description Create or update the \code{NEWS} file. It will include a template
+#' @title Update "NEWS.md"
+#' @description Create or update the "NEWS.md" file. It will include a template
 #'    for new sections if relevant, and update the version number.
 #'
 #' @details Default is \code{template = NULL} that will only update the version number.
-#'     Use \code{template = "first"} to make the \code{NEWS} template for the first
-#'     release of the package. Use \code{template = "develop"} to make the \code{NEWS}
+#'     Use \code{template = "first"} to make the "NEWS.md" template for the first
+#'     release of the package. Use \code{template = "develop"} to make the "NEWS.md"
 #'     template for the successive releases of the package.
 #'
 #' @template pkg
 #' @template pkg_path
 #' @param template [\code{character(1)}]\cr
-#' The template to add to the \code{NEWS} file. Must be one of
+#' The template to add to the "NEWS.md" file. Must be one of
 #'     c("first", "develop"). Defaults to \code{NULL}.
 #'
-#' @return None. Creates or updates the \code{NEWS} file in the package directory.
+#' @return None. Creates or updates the "NEWS.md" file in the package directory.
 #'
 #' @author Petter Hopp Petter.Hopp@@vetinst.no
 #' @export
 #' @examples
 #' \dontrun{
-#' # Create the first NEWS file
+#' # Create the first "NEWS.md" file
 #' library(NVIpackager)
 #' update_news(template = "first")
 #' }
@@ -39,13 +39,18 @@ update_news <- function(pkg = stringi::stri_extract_last_words(usethis::proj_pat
   # RUN SCRIPT ----
 
   # Fetch version and date from DESCRIPTION and generate header
-  header <- paste(paste(desc::desc_get_field(key = "Package"),
+  header <- paste(paste("#",
+                        desc::desc_get_field(key = "Package"),
                         desc::desc_get_field(key = "Version"),
                         paste0("- (", desc::desc_get_field(key = "Date"), ")")))
 
   # If NEWS exist read in file
-  if (file.exists(file.path(pkg_path, "NEWS"))) {
-    history <- readLines(con = file.path(pkg_path, "NEWS"))
+  NEWS_file <- NULL
+  if (file.exists(file.path(pkg_path, "NEWS"))) {NEWS_file <- "NEWS"}
+  if (file.exists(file.path(pkg_path, "NEWS.md"))) {NEWS_file <- "NEWS.md"}
+
+  if (!is.null(NEWS_file)) {
+    history <- readLines(con = file.path(pkg_path, NEWS_file))
   } else {
     history <- ""
   }
@@ -53,10 +58,9 @@ update_news <- function(pkg = stringi::stri_extract_last_words(usethis::proj_pat
   # If template = "first", then make first template for NEWS in a package. This
   #    is only relevant when the file NEWS doesn't exist.
   if (!is.null(template) && template == "first") {
-    if (!file.exists(file.path(pkg_path, "NEWS"))) {
-      template_text <- c("----------------------------------------",
-                         "",
-                         "First release:",
+    if (is.null(NEWS_file)) {
+      template_text <- c("",
+                         "## First release:",
                          "",
                          paste0(desc::desc_get_field(key = "Title"), ":"),
                          "",
@@ -67,24 +71,23 @@ update_news <- function(pkg = stringi::stri_extract_last_words(usethis::proj_pat
   # If template = "develop" update with development version number and include
   #     headlines for potential changes.
   if (!is.null(template) && template == "develop") {
-    template_text <- c("----------------------------------------",
-                       "",
-                       "New features:",
-                       "",
-                       "-",
-                       "",
-                       "",
-                       "Bug fixes:",
+    template_text <- c("",
+                       "## New features:",
                        "",
                        "-",
                        "",
                        "",
-                       "Other changes:",
+                       "## Bug fixes:",
                        "",
                        "-",
                        "",
                        "",
-                       "BREAKING CHANGES:",
+                       "## Other changes:",
+                       "",
+                       "-",
+                       "",
+                       "",
+                       "## BREAKING CHANGES:",
                        "",
                        "-",
                        "",
@@ -95,9 +98,9 @@ update_news <- function(pkg = stringi::stri_extract_last_words(usethis::proj_pat
   if (is.null(template)) {
     history <- history[2:length(history)]
     writeLines(text = c(header, history),
-               con = file.path(pkg_path, "NEWS"))
+               con = file.path(pkg_path, NEWS_file))
   } else {
     writeLines(text = c(header, template_text, history),
-               con = file.path(pkg_path, "NEWS"))
+               con = file.path(pkg_path, NEWS_file))
   }
 }
